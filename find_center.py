b34f14995d60c004983243777ee4d42e1f5253e9
tfvankuik <t.f.vankuik@student.tudelft.nl>
2025-02-13 16:54:38 +0100

    finished find_center function

--- find_center.py
+++ find_center.py
@@ -1,34 +1,38 @@
 import cv2
 import numpy as np
+def find_center(img): 
+    # convert image to grayscale image
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # convert the grayscale image to binary image
+    ret,bin_img = cv2.threshold(gray_image,127,255,0)
 
-img = cv2.imread('test_picture.jpg', cv2.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-imgS = cv2.resize(img, (960, 540))
+    # calculate moments of binary image
+    M = cv2.moments(bin_img)
+    
+    # calculate x,y coordinate of center
+    centroid_x = int(M["m10"] / M["m00"])
+    centroid_y = int(M["m01"] / M["m00"])
+    
+    # put text and highlight the center
+    cv2.circle(bin_img, (centroid_x, centroid_y), 5, (0, 0, 0), -1)
+    
+    # display the image
+    cv2.imshow("Image", bin_img)
+    cv2.waitKey(0)
+    
+    # find x and y coordinate of center picture
+    dimensions = bin_img.shape
+    img_center_x = dimensions[1]/2
+    img_center_y = dimensions[0]/2
+    print(centroid_x, img_center_x)
+    print(centroid_y, img_center_y)
+    # calculate distance between centroid and center image
+    x_distance = centroid_x-img_center_x
+    y_distance = centroid_y-img_center_y
+    return x_distance, y_distance
 
- 
-# convert the grayscale image to binary image
-ret,thresh = cv2.threshold(imgS,127,255,0)
-
-img_inv = cv2.bitwise_not(thresh)
-# calculate moments of binary image
-M = cv2.moments(img_inv)
- 
-# calculate x,y coordinate of center
-centroid_x = int(M["m10"] / M["m00"])
-centroid_y = int(M["m01"] / M["m00"])
- 
-# put text and highlight the center
-cv2.circle(img_inv, (centroid_x, centroid_y), 5, (0, 0, 0), -1)
-cv2.putText(img_inv, "centroid", (centroid_x - 25, centroid_y - 25),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2)
- 
-# display the image
-cv2.imshow("Image", img_inv)
-cv2.waitKey(0)
-dimensions = img_inv.shape
-img_center_x = dimensions[0]/2
-img_center_y = dimensions[1]/2
-
-x_distance = centroid_x-img_center_x
-y_distance = centroid_y-img_center_y
-print(centroid_x, img_center_x, x_distance)
-print(centroid_y, img_center_y, y_distance)
+if __name__ == '__main__':
+    img = cv2.imread("test_picture.jpg")
+    img_resize = cv2.resize(img, (960,540))
+    img_invert = cv2.bitwise_not(img_resize)
+    find_center(img_invert)
